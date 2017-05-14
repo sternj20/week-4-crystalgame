80ebd6b057463a5d89143a8580b83fc1e60bf956
sternj20 <jessesternmusic@gmail.com>
2017-05-13 22:43:39 -0700

    finished up logic. need to make it look prettier

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,8 @@
 //global variables
 var totalScore = 0;
 var numToGuess = 0;
+var wins = 0;
+var losses = 0;
 var purpleCrysNum = 0;
 var crazyCrysNum = 0;
 var blueCrysNum = 0;
@@ -24,9 +26,15 @@ var crazyCrys = $("#crazycrys");
 var blueCrys = $("#bluecrys");
 var whiteCrys = $("#whitecrys");
 var crystalImgs = $(".crystalimgs");
+var scorenums = $("#scorenums");
+var winsHtml = $("#wins");
+var lossesHtml = $("#losses");
 
 //functions
 function gameStart(){
+	//clear total score
+	totalScore = 0;
+	scorenums.text(totalScore);
 	//generate a random number each time the game starts between 19-120
 	//assign this random number to variable numToGuess
 	numToGuess = Math.floor(Math.random()*(120-19+1)+19);
@@ -43,16 +51,37 @@ function gameStart(){
 	console.log(blueCrys.attr("data-crystalvalue"));
 	console.log(whiteCrys.attr("data-crystalvalue"));
 }
-
-
 	//event listener for each crystal
 	//when crystal is clicked you add the random number assigned to that crystal to your score
 	crystalImgs.on('click', function() {
 		totalScore += parseInt($(this).attr("data-crystalvalue"));
-		$("#scorenums").text(totalScore);
+		scorenums.text(totalScore);
 		console.log($(this).attr("data-crystalvalue"));
 		console.log (totalScore);
+		checkWinner();
 	});
+	
+//if statement
+
+function checkWinner(){
+	//if your total score equals the random number
+		// you win
+		if(totalScore === numToGuess){
+			console.log('you win');
+			wins++;
+			winsHtml.text(wins);
+			$("#outcome").text('You win!');
+			gameStart();
+	//else if your total score is greater than random number
+		//you lose 
+	} else if (numToGuess < totalScore) {
+		console.log('you lose');
+		losses++;
+		lossesHtml.text(losses);
+		$("#outcome").text('You lose');
+		gameStart();
+	}
+}
 
 //calls 
 
